03af2a3b2ed64aa2d749515d2d33751c34b9d130
Chan Nundoo <chan.nundoo@gmail.com>
2023-09-06 22:55:21 +0100

    Threadpool - increasing min number of threads

---
+++ ThreadpoolExperiment/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Increasing number of threadpool threads");
+
+
+int minWorkerThreads, completionPortThreads;
+ThreadPool.GetMinThreads(out minWorkerThreads, out completionPortThreads);
+DisplayCounts(minWorkerThreads, completionPortThreads);
+
+ThreadPool.SetMinThreads(minWorkerThreads + 10, completionPortThreads);
+
+ThreadPool.GetMinThreads(out minWorkerThreads, out completionPortThreads);
+DisplayCounts(minWorkerThreads, completionPortThreads);
+
+ThreadPool.GetAvailableThreads(out minWorkerThreads, out completionPortThreads);
+DisplayCounts(minWorkerThreads, completionPortThreads);
+void DisplayCounts(int workerThreads, int completionPortThreads)
+{
+    Console.WriteLine("Number of worker threads: " + workerThreads);
+    Console.WriteLine("Number of I/O completion threads: " + completionPortThreads);
+}
